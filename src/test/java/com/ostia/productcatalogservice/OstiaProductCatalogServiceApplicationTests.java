50e31caca2a9175e27ff34db21e8f8b42edfdd5b
Hamza W. Amentag <william.amentag@gmail.com>
2025-06-27 11:34:59 +0100

    Initial commit

---
+++ src/test/java/com/ostia/productcatalogservice/OstiaProductCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ostia.productcatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OstiaProductCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
