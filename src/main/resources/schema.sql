193fcb0f26c5006e2a8465051f96f12ce8186647
arberbakalli <arberbakalli@gmail.com>
2025-07-09 22:07:44 +0200

    feat: add employees table to schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,3 +6,13 @@ CREATE TABLE CATEGORY (
                           name VARCHAR(255) NOT NULL UNIQUE,
                           description VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE EMPLOYEES (
+                          id UUID PRIMARY KEY,
+                          version INT NOT NULL,
+                          created_at TIMESTAMP NOT NULL,
+                          updated_at TIMESTAMP NOT NULL,
+                          first_name VARCHAR(255) NOT NULL,
+                          last_name VARCHAR(255) NOT NULL,
+                          email VARCHAR(255) NOT NULL UNIQUE
+);
